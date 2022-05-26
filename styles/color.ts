7c3ffbbbcfefda1b2de41d127c81b211a5b33848
jiwon <jidole041214@naver.com>
2022-05-26 09:51:32 +0900

    add init

---
+++ styles/color.ts
@@ -0,0 +1,9 @@
+export const COLOR = {
+  main_text: "#424242",
+  sub_text: "#616161",
+  input: "#BDBDBD",
+  red100: "#FF8888",
+  red200: "#FF1A1A",
+  main: "#5B56FF",
+  border: "#9E9E9E",
+};
