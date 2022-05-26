8fa62b6d0e6e33aa7e93a75c983a428088dddf7d
jiwon <jidole041214@naver.com>
2022-05-26 11:50:18 +0900

    update 디자인 변경

--- components/common/Modal/login.tsx
+++ components/common/Modal/login.tsx
@@ -4,14 +4,13 @@ import styled from "@emotion/styled";
 import { COLOR } from "./../../../styles/color";
 
 const Container = styled.div`
-  width: 450px;
-  height: 250px;
+  width: 420px;
+  padding: 30px 0;
   background-color: white;
-  border-radius: 20px;
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 25px;
   & h3 {
     font-size: 20px;
     font-weight: 500;
@@ -20,23 +19,27 @@ const Container = styled.div`
   button {
     width: 350px;
     height: 50px;
-    font-size: 16px;
     border-radius: 5px;
   }
   & input {
-    margin-top: 30px;
+    margin-top: 20px;
     border: 1px solid ${COLOR.input};
     color: ${COLOR.input};
     padding-left: 15px;
+    font-size: 14px;
     &::placeholder {
       color: ${COLOR.input};
     }
+    &:nth-of-type(2) {
+      margin-top: 10px;
+    }
   }
   & button {
-    margin-top: 10px;
+    margin-top: 15px;
     background-color: ${COLOR.main};
     color: ${COLOR.white};
     font-weight: bold;
+    font-size: 16px;
   }
 `;
 
@@ -46,7 +49,8 @@ export const Login = (props: Props) => {
   return (
     <Container>
       <h3>기업 로그인 하기</h3>
-      <input type="text" placeholder="사업자 번호를 입력하세요 ex) 000-000  " />
+      <input type="text" placeholder="사업자 번호를 입력하세요 ex) 000-000" />
+      <input type="text" placeholder="비밀번호를 입력하세요" />
       <button>로그인</button>
     </Container>
   );
